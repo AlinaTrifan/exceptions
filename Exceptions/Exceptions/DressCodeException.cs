5db55a6e456076e6d321280e9e959d30bbb3cf33
AlinaTrifan <isacnalina@yahoo.com>
2018-12-15 12:17:08 +0200

    added custom exceptions

---
+++ Exceptions/Exceptions/DressCodeException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exceptions
+{
+    public class DressCodeException :ApplicationException
+    {
+        private readonly string message = "you are not wearing an elf costume and you are under age";
+        public DressCodeException()
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
